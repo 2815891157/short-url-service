077eb64eecfa7f327c9d1385d499b3bd3f77ed66
2815891157 <2815891157@users.noreply.github.com>
2026-07-25 02:38:43 +0800

    refactor: 零配置全自动
    - 首次访问自动弹出数据库配置页面，填完即用
    - 不需要改任何代码，不需要手动建表
    - 去掉 setup.php / router.php，精简到 11 个文件
    - 所有文件同一目录，直接上传即用

--- router.php
+++
@@ -1,8 +0,0 @@
-<?php
-// 本地测试: php -S localhost:8000 router.php
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-if (preg_match('#^/s/(.+)$#', $uri, $m)) { $_GET['slug'] = $m[1]; require __DIR__ . '/s.php'; return true; }
-if (preg_match('#^/api(/.*)?$#', $uri)) { require __DIR__ . '/api.php'; return true; }
-$file = __DIR__ . $uri;
-if ($uri !== '/' && is_file($file)) return false;
-return false;
